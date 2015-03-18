265b4f8f5f175a5c5cb38f24838806c331e4268d
overmind <seogarry@gmail.com>
2015-03-18 13:10:37 +0300

    add google plus links support, add charset header detection

--- src/OpenGraph.php
+++ src/OpenGraph.php
@@ -17,41 +17,47 @@
 	Original can be found at https://github.com/scottmac/opengraph/blob/master/OpenGraph.php
    
 */
-
+/**
+ * Class OpenGraph
+ * @description based on scottmac opengraph class, modification with charset detection and php xml hacks
+ */
 class OpenGraph implements Iterator
 {
-  /**
-   * There are base schema's based on type, this is just
-   * a map so that the schema can be obtained
-   *
-   */
-	public static $TYPES = array(
-		'activity' => array('activity', 'sport'),
-		'business' => array('bar', 'company', 'cafe', 'hotel', 'restaurant'),
-		'group' => array('cause', 'sports_league', 'sports_team'),
-		'organization' => array('band', 'government', 'non_profit', 'school', 'university'),
-		'person' => array('actor', 'athlete', 'author', 'director', 'musician', 'politician', 'public_figure'),
-		'place' => array('city', 'country', 'landmark', 'state_province'),
-		'product' => array('album', 'book', 'drink', 'food', 'game', 'movie', 'product', 'song', 'tv_show'),
-		'website' => array('blog', 'website'),
-	);
+    /**
+     * There are base schema's based on type, this is just
+     * a map so that the schema can be obtained
+     *
+     */
+    public static $TYPES = array(
+        'activity' => array('activity', 'sport'),
+        'business' => array('bar', 'company', 'cafe', 'hotel', 'restaurant'),
+        'group' => array('cause', 'sports_league', 'sports_team'),
+        'organization' => array('band', 'government', 'non_profit', 'school', 'university'),
+        'person' => array('actor', 'athlete', 'author', 'director', 'musician', 'politician', 'public_figure'),
+        'place' => array('city', 'country', 'landmark', 'state_province'),
+        'product' => array('album', 'book', 'drink', 'food', 'game', 'movie', 'product', 'song', 'tv_show'),
+        'website' => array('blog', 'website'),
+    );
 
-  /**
-   * Holds all the Open Graph values we've parsed from a page
-   *
-   */
-	private $_values = array();
+    /**
+     * Holds all the Open Graph values we've parsed from a page
+     *
+     */
+    private $_values = array();
 
-  /**
-   * Fetches a URI and parses it for Open Graph data, returns
-   * false on error.
-   *
-   * @param $URI    URI to page to parse for Open Graph data
-   * @return OpenGraph
-   */
-	static public function fetch($URI) {
+    /**
+     * Fetches a URI and parses it for Open Graph data, returns
+     * false on error.
+     *
+     * @param $URI    URI to page to parse for Open Graph data
+     * @return OpenGraph
+     */
+    static public function fetch($URI)
+    {
+        $URI = str_replace(' ', '+', $URI);
         $curl = curl_init($URI);
 
+        //curl_setopt($curl, CURLOPT_URL, rawurlencode($URI));
         curl_setopt($curl, CURLOPT_FAILONERROR, true);
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -59,186 +65,287 @@ class OpenGraph implements Iterator
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-	curl_setopt($curl, CURLOPT_ENCODING , "gzip");
-	curl_setopt($curl, CURLOPT_COOKIEJAR, '/tmp/opengraph_curl_cookies.txt');
+        curl_setopt($curl, CURLOPT_ENCODING , "gzip");
+        curl_setopt($curl, CURLOPT_HEADER, true);
+        curl_setopt($curl, CURLOPT_COOKIEJAR, '/tmp/opengraph_curl_cookies.txt');
         curl_setopt($curl, CURLOPT_COOKIEFILE, '/tmp/opengraph_curl_cookies.txt');
 
 
         $response = curl_exec($curl);
 
+        $header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+        $headers_string = substr($response, 0, $header_size);
+        $body = substr($response, $header_size);
+        $headers  = self::parseHeaders($headers_string);
+
         curl_close($curl);
 
-        if (!empty($response)) {
-            return self::_parse($response);
-        } else {
+        if (!empty($body))
+        {
+            return self::_parse($body, $headers);
+        }
+        return false;
+    }
+
+    /**
+     * @param $headers_string
+     * @return array
+     */
+    static public function parseHeaders($headers_string)
+    {
+        $headers = explode("\n", $headers_string);
+        $data = [];
+        foreach ($headers as $h)
+        {
+            $t = explode(":", $h);
+            $data[$t[0]] = $t[1];
+        }
+        return $data;
+    }
+
+
+    /**
+     * @param $headers
+     * @return bool
+     */
+    static public function getCharset($headers)
+    {
+        $content_type = $headers['Content-Type'];
+        $t = explode(";", $content_type);
+        if (is_array($t))
+        {
+            foreach ($t as $p)
+            {
+                if (strstr($p, "charset"))
+                {
+                    $h = explode("=", $p);
+                    return trim($h[1]);
+                }
+            }
+        }
+        return false;
+
+    }
+
+    /**
+     * Parses HTML and extracts Open Graph data, this assumes
+     * the document is at least well formed.
+     *
+     * @param $HTML    HTML to parse
+     * @return OpenGraph
+     */
+    static private function _parse($HTML, $headers)
+    {
+        $headerCharset = self::getCharset($headers);
+
+        $detected = false;
+
+        if ($headerCharset == 'koi8-r')
+        {
+            $HTML = mb_convert_encoding($HTML, 'UTF-8', 'KOI8-R');
+            $detected = true;
+        }
+
+        if ($headerCharset == 'cp1251')
+        {
+            $HTML = mb_convert_encoding($HTML, 'UTF-8', 'CP1251');
+            $detected = true;
+        }
+
+        if (!$detected)
+        {
+            preg_match('/<meta charset=[\"|\'](.*?)[\"|\']/i', $HTML, $matches);
+
+            if ( $matches[1] && 'utf-8' != strtolower($matches[1]) )
+            {
+                $encoding = $matches[1];
+                $HTML = mb_convert_encoding($HTML, 'UTF-8', $encoding);
+            }
+
+            preg_match('/<meta http\-equiv\=\"Content-Type\" content=\"text\/html\; charset\=(.*?)\">/i', $HTML, $matches);
+
+            if ( $matches[1] && 'utf-8' != strtolower($matches[1]) )
+            {
+                $encoding = $matches[1];
+                $HTML = mb_convert_encoding($HTML, 'UTF-8', $encoding);
+            }
+
+            $HTML = preg_replace('/\<meta charset\=\"(.*?)\"/i',"<meta charset='utf-8'", $HTML );
+
+            preg_match('/<meta http-equiv="content-type"/i', $HTML, $m);
+
+            // if ( strpos($HTML, '<meta http-equiv="content-type"') === false ) :
+            if (!isset($m[0]))
+            {
+                $HTML = preg_replace( '/<head(.*?)>/i', '<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">', $HTML );//die("12312");
+            }
+            else
+            {
+                $HTML = preg_replace( '/<meta http\-equiv\=\"Content-Type\" content=\"text\/html\; charset\=(.*?)\">/i', '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">', $HTML );
+            }
+        }
+        else
+        {
+            $HTML = preg_replace( '/<head(.*?)>/i', '<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">', $HTML );
+        }
+
+
+
+        $old_libxml_error = libxml_use_internal_errors(true);
+
+        $doc = new DOMDocument();
+        // dirty hack for dirty shit from http://stackoverflow.com/questions/8218230/php-domdocument-loadhtml-not-encoding-utf-8-correctly
+        $doc->loadHTML('<?xml encoding="utf-8" ?>' . $HTML);
+
+        libxml_use_internal_errors($old_libxml_error);
+
+        $tags = $doc->getElementsByTagName('meta');
+        if (!$tags || $tags->length === 0)
+        {
             return false;
         }
-	}
 
-  /**
-   * Parses HTML and extracts Open Graph data, this assumes
-   * the document is at least well formed.
-   *
-   * @param $HTML    HTML to parse
-   * @return OpenGraph
-   */
-	static private function _parse($HTML) {
-                preg_match('/<meta charset=[\"|\'](.*?)[\"|\']/i', $HTML, $matches);
+        $page = new self();
 
-                if ( $matches[1] && 'utf-8' != strtolower($matches[1]) ) :
-                        $encoding = $matches[1];
-                        $HTML = mb_convert_encoding($HTML, 'UTF-8', $encoding);
-                endif;
+        $nonOgDescription = null;
 
-                preg_match('/<meta http\-equiv\=\"Content-Type\" content=\"text\/html\; charset\=(.*?)\">/i', $HTML, $matches);
+        foreach ($tags AS $tag)
+        {
+            if ($tag->hasAttribute('property') &&
+                strpos($tag->getAttribute('property'), 'og:') === 0) {
+                $key = strtr(substr($tag->getAttribute('property'), 3), '-', '_');
+                $page->_values[$key] = $tag->getAttribute('content');
+            }
 
-                if ( $matches[1] && 'utf-8' != strtolower($matches[1]) ) :
-                        $encoding = $matches[1];
-                        $HTML = mb_convert_encoding($HTML, 'UTF-8', $encoding);
-                endif;
-
-                $HTML = preg_replace('/\<meta charset\=\"(.*?)\"/i',"<meta charset='utf-8'", $HTML );
-
-                preg_match('/<meta http-equiv="content-type"/i', $HTML, $m);
-
-                // if ( strpos($HTML, '<meta http-equiv="content-type"') === false ) :
-                if (!isset($m[0])):
-                        $HTML = preg_replace( '/<head(.*?)>/i', '<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">', $HTML );
-                else :
-                        $HTML = preg_replace( '/<meta http\-equiv\=\"Content-Type\" content=\"text\/html\; charset\=(.*?)\">/i', '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">', $HTML );
-                endif;
-
-		
-		$old_libxml_error = libxml_use_internal_errors(true);
-
-		$doc = new DOMDocument();
-		// dirty hack for dirty shit from http://stackoverflow.com/questions/8218230/php-domdocument-loadhtml-not-encoding-utf-8-correctly
-		$doc->loadHTML('<?xml encoding="utf-8" ?>' . $HTML);
-		
-		libxml_use_internal_errors($old_libxml_error);
-
-		$tags = $doc->getElementsByTagName('meta');
-		if (!$tags || $tags->length === 0) {
-			return false;
-		}
-
-		$page = new self();
-
-		$nonOgDescription = null;
-		
-		foreach ($tags AS $tag) {
-			if ($tag->hasAttribute('property') &&
-			    strpos($tag->getAttribute('property'), 'og:') === 0) {
-				$key = strtr(substr($tag->getAttribute('property'), 3), '-', '_');
-				$page->_values[$key] = $tag->getAttribute('content');
-			}
-			
-			//Added this if loop to retrieve description values from sites like the New York Times who have malformed it. 
-			if ($tag ->hasAttribute('value') && $tag->hasAttribute('property') &&
-			    strpos($tag->getAttribute('property'), 'og:') === 0) {
-				$key = strtr(substr($tag->getAttribute('property'), 3), '-', '_');
-				$page->_values[$key] = $tag->getAttribute('value');
-			}
-			//Based on modifications at https://github.com/bashofmann/opengraph/blob/master/src/OpenGraph/OpenGraph.php
-			if ($tag->hasAttribute('name') && $tag->getAttribute('name') === 'description') {
+            //Added this if loop to retrieve description values from sites like the New York Times who have malformed it.
+            if ($tag ->hasAttribute('value') && $tag->hasAttribute('property') &&
+                strpos($tag->getAttribute('property'), 'og:') === 0) {
+                $key = strtr(substr($tag->getAttribute('property'), 3), '-', '_');
+                $page->_values[$key] = $tag->getAttribute('value');
+            }
+            //Based on modifications at https://github.com/bashofmann/opengraph/blob/master/src/OpenGraph/OpenGraph.php
+            if ($tag->hasAttribute('name') && $tag->getAttribute('name') === 'description')
+            {
                 $nonOgDescription = $tag->getAttribute('content');
             }
-			
-		}
-		//Based on modifications at https://github.com/bashofmann/opengraph/blob/master/src/OpenGraph/OpenGraph.php
-		if (!isset($page->_values['title'])) {
+
+        }
+        //Based on modifications at https://github.com/bashofmann/opengraph/blob/master/src/OpenGraph/OpenGraph.php
+        if (!isset($page->_values['title']))
+        {
             $titles = $doc->getElementsByTagName('title');
-            if ($titles->length > 0) {
+            if ($titles->length > 0)
+            {
                 $page->_values['title'] = $titles->item(0)->textContent;
             }
         }
-        if (!isset($page->_values['description']) && $nonOgDescription) {
+
+        if (!isset($page->_values['description']) && $nonOgDescription)
+        {
             $page->_values['description'] = $nonOgDescription;
         }
 
         //Fallback to use image_src if ogp::image isn't set.
-        if (!isset($page->values['image'])) {
+        if (!isset($page->values['image']))
+        {
             $domxpath = new DOMXPath($doc);
             $elements = $domxpath->query("//link[@rel='image_src']");
 
-            if ($elements->length > 0) {
+            if ($elements->length > 0)
+            {
                 $domattr = $elements->item(0)->attributes->getNamedItem('href');
-                if ($domattr) {
+                if ($domattr)
+                {
                     $page->_values['image'] = $domattr->value;
                     $page->_values['image_src'] = $domattr->value;
                 }
             }
         }
 
-		if (empty($page->_values)) { return false; }
-		
-		return $page;
-	}
+        if (empty($page->_values)) { return false; }
 
-  /**
-   * Helper method to access attributes directly
-   * Example:
-   * $graph->title
-   *
-   * @param $key    Key to fetch from the lookup
-   */
-	public function __get($key) {
-		if (array_key_exists($key, $this->_values)) {
-			return $this->_values[$key];
-		}
-		
-		if ($key === 'schema') {
-			foreach (self::$TYPES AS $schema => $types) {
-				if (array_search($this->_values['type'], $types)) {
-					return $schema;
-				}
-			}
-		}
-	}
+        return $page;
+    }
 
-  /**
-   * Return all the keys found on the page
-   *
-   * @return array
-   */
-	public function keys() {
-		return array_keys($this->_values);
-	}
+    /**
+     * Helper method to access attributes directly
+     * Example:
+     * $graph->title
+     *
+     * @param $key    Key to fetch from the lookup
+     * @param $key
+     * @return int|string
+     */
+    public function __get($key)
+    {
+        if (array_key_exists($key, $this->_values))
+        {
+            return $this->_values[$key];
+        }
 
-  /**
-   * Helper method to check an attribute exists
-   *
-   * @param $key
-   */
-	public function __isset($key) {
-		return array_key_exists($key, $this->_values);
-	}
+        if ($key === 'schema')
+        {
+            foreach (self::$TYPES AS $schema => $types)
+            {
+                if (array_search($this->_values['type'], $types))
+                {
+                    return $schema;
+                }
+            }
+        }
+    }
 
-  /**
-   * Will return true if the page has location data embedded
-   *
-   * @return boolean Check if the page has location data
-   */
-	public function hasLocation() {
-		if (array_key_exists('latitude', $this->_values) && array_key_exists('longitude', $this->_values)) {
-			return true;
-		}
-		
-		$address_keys = array('street_address', 'locality', 'region', 'postal_code', 'country_name');
-		$valid_address = true;
-		foreach ($address_keys AS $key) {
-			$valid_address = ($valid_address && array_key_exists($key, $this->_values));
-		}
-		return $valid_address;
-	}
+    /**
+     * Return all the keys found on the page
+     *
+     * @return array
+     */
+    public function keys()
+    {
+        return array_keys($this->_values);
+    }
 
-  /**
-   * Iterator code
-   */
-	private $_position = 0;
-	public function rewind() { reset($this->_values); $this->_position = 0; }
-	public function current() { return current($this->_values); }
-	public function key() { return key($this->_values); }
-	public function next() { next($this->_values); ++$this->_position; }
-	public function valid() { return $this->_position < sizeof($this->_values); }
+    /**
+     * Helper method to check an attribute exists
+     *
+     * @param $key
+     * @return bool
+     */
+    public function __isset($key)
+    {
+        return array_key_exists($key, $this->_values);
+    }
+
+    /**
+     * Will return true if the page has location data embedded
+     *
+     * @return boolean Check if the page has location data
+     */
+    public function hasLocation()
+    {
+        if (array_key_exists('latitude', $this->_values) && array_key_exists('longitude', $this->_values))
+        {
+            return true;
+        }
+
+        $address_keys = array('street_address', 'locality', 'region', 'postal_code', 'country_name');
+        $valid_address = true;
+
+        foreach ($address_keys AS $key)
+        {
+            $valid_address = ($valid_address && array_key_exists($key, $this->_values));
+        }
+
+        return $valid_address;
+    }
+
+    /**
+     * Iterator code
+     */
+    private $_position = 0;
+    public function rewind() { reset($this->_values); $this->_position = 0; }
+    public function current() { return current($this->_values); }
+    public function key() { return key($this->_values); }
+    public function next() { next($this->_values); ++$this->_position; }
+    public function valid() { return $this->_position < sizeof($this->_values); }
 }
